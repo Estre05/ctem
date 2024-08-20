6b01a30a49ed1f39090a4e7f0a5dc850fef10906
Alexnarea <ebgarcia@sudamericano.edu.ec>
2024-08-20 10:22:58 -0500

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ import { ThemesComponent } from './pages/themes/themes.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
-  { path: 'information', loadComponent: () => import('./pages/information/information.component').then(m => m.InformationComponent) },
+  { path: 'information', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
+  { path: 'home', loadComponent: () => import('./pages/information/information.component').then(m => m.InformationComponent) },
   { path: 'themes', loadComponent: () => import('./pages/themes/themes.component').then(m => m.ThemesComponent) }
   /*{ path: 'ponentes', loadComponent: () => import('./themes/themes.component').then(m => m.ThemesComponent) }
     { path: 'unete', loadComponent: () => import('./themes/themes.component').then(m => m.ThemesComponent) }
